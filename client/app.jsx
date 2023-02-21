de571b67becd064eaf6da3da5fd611d1a37fc1c3
Travis Collins <travis@tec27.com>
2023-02-20 19:04:35 -0800

    Move settings behind snackbars + dialogs. (I think this was fine before because they use Portal but it makes more logical sense this way imo)

--- client/app.jsx
+++ client/app.jsx
@@ -113,9 +113,9 @@ export default function App() {
                   <MainContent />
                 </Route>
               </Switch>
+              <Settings />
               <ConnectedSnackbar />
               <ConnectedDialogOverlay />
-              <Settings />
             </React.Suspense>
           </RootErrorBoundary>
           <UpdateOverlay />
